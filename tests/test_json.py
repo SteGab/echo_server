90474e06ad69ba9b71659f4b453bb2c74f16c8d4
STG <stg>
2026-06-01 16:14:59 +0200

    some tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -10,15 +10,26 @@ def test_send():
         "value2": "No2"
     }
     
-    #nestedString = json.dumps(nestedObj).replace("'", "\"")
     nestedString = json.dumps(nestedObj)
     
-    payload = {
+    payload_obj = {
         "Param1": "Parameter1",
         "nested": nestedString
     }
-    response = requests.post(url="http://localhost:8000/echo-post", json=json.dumps(payload))
+    payload_str = json.dumps(payload_obj)
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    
+    response = requests.post(url="http://localhost:8000/echo-post", json=payload_str, headers=headers)
+    
+    
     assert response.status_code == 200
+    
+    response_raw = response.text
+    logging.info(f"Response Raw: {response_raw}")
     response_json = response.json()
     #logging.info(f"Response JSON: {response_json}")
     logging.info(f"Response JSON: {response_json['received_data']}")
